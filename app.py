d163f8bde8cdafba2c0b790e3e53fbb83baa82be
manilesrun <srunmanile@gmail.com>
2026-04-08 13:50:01 +1000

    feat: api get detections

---
+++ app.py
@@ -0,0 +1,40 @@
+import threading
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from main_testcode import simulate_radar_data
+from routers import detections
+import uvicorn
+import testData as td
+
+
+def create_app():
+    app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            "http://localhost:5173",
+        ],  # Who can call your API?
+        allow_methods=["*"],  # What HTTP methods are allowed?
+        allow_headers=["*"],  # What headers can be sent?
+        # "*" means anyone
+    )
+
+    app.include_router(detections.router, tags=["Detections"])
+
+    @app.on_event("startup")
+    # thread is used to run the simulation background
+    def start_simulation():
+        thread = threading.Thread(
+            target=simulate_radar_data, args=(td.Test_radar_data,), daemon=True
+        )
+        thread.start()
+
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
